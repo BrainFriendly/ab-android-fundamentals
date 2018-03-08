fd11278d712416d881315d26014daed87e594d41
Eduardo Medina <emedinaa@gmail.com>
2018-03-08 13:23:53 -0500

    Add exercises

--- FragmentsStarted/app/src/main/java/com/androidbootcamp/androidtemplate/ui/listeners/OnColorListener.java
+++
@@ -1,9 +0,0 @@
-package com.androidbootcamp.androidtemplate.ui.listeners;
-
-/**
- * Created by eduardomedina on 31/01/17.
- */
-public interface OnColorListener {
-
-    void seleccionarColor(int pos);
-}
